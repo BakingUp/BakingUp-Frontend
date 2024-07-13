d12f231ef77fc7c09c5b3d63af485fda28e35610
Pawin Nakvisai <pawinnak2@gmail.com>
2024-07-13 15:11:06 +0700

    Refactor ingredient detail screen part 2

---
+++ lib/components/ingredient_detail/ingredient_stock_detail_edit_button.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class IngredientStockDetailEditButton extends StatelessWidget {
+  const IngredientStockDetailEditButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Image.asset('assets/icons/edit.png');
+  }
+}
--- lib/screens/ingredient_detail_screen.dart
+++ lib/screens/ingredient_detail_screen.dart
@@ -1,4 +1,6 @@
 // Importing libraries
+import 'package:bakingup_frontend/components/ingredient_detail/ingredient_detail_container.dart';
+import 'package:bakingup_frontend/components/ingredient_detail/ingredient_stock_detail_edit_button.dart';
 import 'package:flutter/material.dart';
 
 // Importing files
@@ -111,63 +113,55 @@ class _IngredientDetailScreenState extends State<IngredientDetailScreen> {
               ),
             ),
           ),
-          Positioned(
-            top: MediaQuery.of(context).size.width / 1.5,
-            left: 0,
-            right: 0,
-            child: Container(
-              decoration: BoxDecoration(
-                color: whiteColor,
-                borderRadius: const BorderRadius.only(
-                  topLeft: Radius.circular(20),
-                  topRight: Radius.circular(20),
-                ),
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.grey.withOpacity(0.3),
-                    spreadRadius: 0.5,
-                    blurRadius: 20,
-                    offset: const Offset(0, 3),
-                  )
-                ],
-              ),
-              child: Container(
-                padding: const EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 20.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
+          IngredientDetailContainer(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.end,
                   children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      crossAxisAlignment: CrossAxisAlignment.end,
+                    Column(
                       children: [
-                        Column(
+                        Row(
                           children: [
-                            Row(
-                              children: [
-                                Text(
-                                  ingredientName,
-                                  style: TextStyle(
-                                    color: blackColor,
-                                    fontFamily: 'Inter',
-                                    fontStyle: FontStyle.normal,
-                                    fontWeight: FontWeight.w500,
-                                    fontSize: 24,
-                                  ),
-                                ),
-                                const Padding(
-                                    padding: EdgeInsets.only(right: 10.0)),
-                                Image.asset('assets/icons/edit.png'),
-                              ],
+                            Text(
+                              ingredientName,
+                              style: TextStyle(
+                                color: blackColor,
+                                fontFamily: 'Inter',
+                                fontStyle: FontStyle.normal,
+                                fontWeight: FontWeight.w500,
+                                fontSize: 24,
+                              ),
                             ),
                             const Padding(
-                                padding: EdgeInsets.only(bottom: 8.0)),
+                              padding: EdgeInsets.only(right: 10.0),
+                            ),
+                            const IngredientStockDetailEditButton(),
                           ],
                         ),
-                        const BakingUpCircularAddButton(),
+                        const Padding(padding: EdgeInsets.only(bottom: 8.0)),
                       ],
                     ),
+                    const BakingUpCircularAddButton(),
+                  ],
+                ),
+                Text(
+                  'Quantity: ${quantity.toString().replaceAll(removeTrailingZeros, '')} $unit',
+                  style: TextStyle(
+                    color: blackColor,
+                    fontFamily: 'Inter',
+                    fontStyle: FontStyle.normal,
+                    fontWeight: FontWeight.w300,
+                    fontSize: 13,
+                  ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
                     Text(
-                      'Quantity: ${quantity.toString().replaceAll(removeTrailingZeros, '')} $unit',
+                      '${ingredientStocks.length} ${ingredientStocks.length > 1 ? "stocks" : "stock"}',
                       style: TextStyle(
                         color: blackColor,
                         fontFamily: 'Inter',
@@ -176,34 +170,19 @@ class _IngredientDetailScreenState extends State<IngredientDetailScreen> {
                         fontSize: 13,
                       ),
                     ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text(
-                          '${ingredientStocks.length} ${ingredientStocks.length > 1 ? "stocks" : "stock"}',
-                          style: TextStyle(
-                            color: blackColor,
-                            fontFamily: 'Inter',
-                            fontStyle: FontStyle.normal,
-                            fontWeight: FontWeight.w300,
-                            fontSize: 13,
-                          ),
-                        ),
-                        Text(
-                          'Notify me : < ${ingredientLessThan.toString().replaceAll(removeTrailingZeros, '')} $unit',
-                          style: TextStyle(
-                            color: blackColor,
-                            fontFamily: 'Inter',
-                            fontStyle: FontStyle.normal,
-                            fontWeight: FontWeight.w300,
-                            fontSize: 13,
-                          ),
-                        ),
-                      ],
-                    )
+                    Text(
+                      'Notify me : < ${ingredientLessThan.toString().replaceAll(removeTrailingZeros, '')} $unit',
+                      style: TextStyle(
+                        color: blackColor,
+                        fontFamily: 'Inter',
+                        fontStyle: FontStyle.normal,
+                        fontWeight: FontWeight.w300,
+                        fontSize: 13,
+                      ),
+                    ),
                   ],
-                ),
-              ),
+                )
+              ],
             ),
           ),
         ],
