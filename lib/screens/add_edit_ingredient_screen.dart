38028f7b879fe948b7b5590199ec8271c7c0eb2f
Pawin Nakvisai <pawinnak2@gmail.com>
2024-08-07 22:01:36 +0700

    [UPDATE] import in add_edit_ingredient_screen

--- lib/screens/add_edit_ingredient_screen.dart
+++ lib/screens/add_edit_ingredient_screen.dart
@@ -9,6 +9,7 @@ import 'package:bakingup_frontend/widgets/add_edit_ingredient/add_edit_ingredien
 import 'package:bakingup_frontend/widgets/add_edit_ingredient/add_edit_ingredient_name_text_field.dart';
 import 'package:bakingup_frontend/widgets/add_edit_ingredient/add_edit_ingredient_title.dart';
 import 'package:bakingup_frontend/widgets/add_edit_ingredient/add_edit_ingredient_notification_text_field.dart';
+import 'package:bakingup_frontend/widgets/baking_up_dialog.dart';
 import 'package:bakingup_frontend/widgets/baking_up_dropdown.dart';
 import 'package:bakingup_frontend/widgets/baking_up_long_action_button.dart';
 
