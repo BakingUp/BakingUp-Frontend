7a1a2cce4b50cb27e4072bddc6531b385bf041f0
Pawin Nakvisai <pawinnak2@gmail.com>
2024-08-07 22:08:52 +0700

    [REMOVE] showDialog in BakingUp Dialog

--- lib/widgets/baking_up_dialog.dart
+++ lib/widgets/baking_up_dialog.dart
@@ -21,72 +21,58 @@ class BakingUpDialog extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    WidgetsBinding.instance.addPostFrameCallback((_) {
-      showDialog(
-        context: context,
-        barrierColor: const Color(0xC7D9D9D9),
-        builder: (BuildContext context) {
-          return AlertDialog(
-            content: Padding(
-              padding:
-                  const EdgeInsets.symmetric(horizontal: 8.0, vertical: 16.0),
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  Text(
-                    title,
-                    style: const TextStyle(
-                      fontSize: 24,
-                      fontFamily: 'Inter',
-                      fontStyle: FontStyle.normal,
-                      fontWeight: FontWeight.w500,
-                    ),
-                    textAlign: TextAlign.center,
-                  ),
-                  const SizedBox(height: 20),
-                  Image.asset(
-                    imgUrl,
-                    width: 60,
-                    height: 60,
-                  ),
-                  const SizedBox(height: 20),
-                  Text(
-                    content,
-                    style: const TextStyle(
-                      fontSize: 16,
-                      fontFamily: 'Inter',
-                      fontStyle: FontStyle.normal,
-                      fontWeight: FontWeight.w400,
-                    ),
-                    textAlign: TextAlign.center,
-                  ),
-                  const SizedBox(height: 30),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      BakingUpLongActionButton(
-                          title: grayButtonTitle, color: greyColor, width: 120),
-                      const SizedBox(width: 8),
-                      secondButtonTitle != null &&
-                              secondButtonTitle?.isNotEmpty == true
-                          ? BakingUpLongActionButton(
-                              title: secondButtonTitle!,
-                              color: secondButtonColor ?? lightGreenColor,
-                              width: 120)
-                          : Container()
-                    ],
-                  )
-                ],
+    return AlertDialog(
+      content: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 16.0),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Text(
+              title,
+              style: const TextStyle(
+                fontSize: 24,
+                fontFamily: 'Inter',
+                fontStyle: FontStyle.normal,
+                fontWeight: FontWeight.w500,
               ),
+              textAlign: TextAlign.center,
             ),
-          );
-        },
-      );
-    });
-
-    return Scaffold(
-      backgroundColor: Colors.transparent,
-      body: Container(),
+            const SizedBox(height: 20),
+            Image.asset(
+              imgUrl,
+              width: 60,
+              height: 60,
+            ),
+            const SizedBox(height: 20),
+            Text(
+              content,
+              style: const TextStyle(
+                fontSize: 16,
+                fontFamily: 'Inter',
+                fontStyle: FontStyle.normal,
+                fontWeight: FontWeight.w400,
+              ),
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(height: 30),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                BakingUpLongActionButton(
+                    title: grayButtonTitle, color: greyColor, width: 120),
+                const SizedBox(width: 8),
+                secondButtonTitle != null &&
+                        secondButtonTitle?.isNotEmpty == true
+                    ? BakingUpLongActionButton(
+                        title: secondButtonTitle!,
+                        color: secondButtonColor ?? lightGreenColor,
+                        width: 120)
+                    : Container()
+              ],
+            )
+          ],
+        ),
+      ),
     );
   }
 }
