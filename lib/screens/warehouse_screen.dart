d7c76ff027980d26a8b46bd37616e64a96c40af6
Nattawat Teaiwtan <Nao.Nait@gmail.com>
2024-09-24 09:33:40 +0700

    [ADD] warehouse filtering

--- lib/screens/warehouse_screen.dart
+++ lib/screens/warehouse_screen.dart
@@ -1,12 +1,13 @@
 import 'package:bakingup_frontend/constants/colors.dart';
+import 'package:bakingup_frontend/enum/expiration_status.dart';
 import 'package:bakingup_frontend/models/warehouse.dart';
 import 'package:bakingup_frontend/services/network_service.dart';
 import 'package:bakingup_frontend/utilities/drawer.dart';
 import 'package:bakingup_frontend/widgets/baking_up_circular_add_button.dart';
+import 'package:bakingup_frontend/widgets/baking_up_filter_modal_bottom.dart';
 import 'package:bakingup_frontend/widgets/baking_up_filter_two_button.dart';
 import 'package:bakingup_frontend/widgets/baking_up_search_bar.dart';
 import 'package:bakingup_frontend/widgets/baking_up_tab_button.dart';
-import 'package:bakingup_frontend/widgets/warehouse/warehouse_ingredient/warehouse_ingredient_filter.dart';
 import 'package:bakingup_frontend/widgets/warehouse/warehouse_ingredient/warehouse_ingredient_list.dart';
 import 'package:bakingup_frontend/widgets/warehouse/warehouse_recipe/warehouse_recipes_list.dart';
 import 'package:flutter/material.dart';
@@ -30,7 +31,22 @@ class _WarehouseScreenState extends State<WarehouseScreen> {
   final TextEditingController _searchRecipeController = TextEditingController();
   final TextEditingController _searchIngredientController =
       TextEditingController();
-
+  final List<String> recipeFilterList = [
+    'Recipe Name',
+    'Total Time',
+    'Serving Amount',
+    'Rating',
+    'Selling Amount'
+  ];
+  final List<String> ingredientFilterList = [
+    'Stock Name',
+    'Quantity',
+    'Expiration Date'
+  ];
+  String selectedRecipeFiltering = "Recipe Name";
+  String selectedRecipeSorting = "Ascending Order";
+  String selectedIngredientFiltering = "Stock Name";
+  String selectedIngredientSorting = "Ascending Order";
   @override
   void initState() {
     super.initState();
@@ -53,6 +69,8 @@ class _WarehouseScreenState extends State<WarehouseScreen> {
       setState(() {
         recipes = data.recipeList;
         filteredRecipes = recipes;
+        selectedRecipeFiltering = "Recipe Name";
+        selectedRecipeSorting = "Ascending Order";
       });
 
       setState(() {});
@@ -83,6 +101,8 @@ class _WarehouseScreenState extends State<WarehouseScreen> {
       setState(() {
         ingredients = data.ingredientList;
         filteredIngredients = ingredients;
+        selectedIngredientFiltering = "Stock Name";
+        selectedIngredientSorting = "Ascending Order";
       });
     } catch (e) {
       setState(() {
@@ -95,7 +115,7 @@ class _WarehouseScreenState extends State<WarehouseScreen> {
     }
   }
 
-  void _filterRecipes(String query) {
+  void _searchRecipes(String query) {
     setState(() {
       if (query == "") {
         filteredRecipes = recipes;
@@ -106,9 +126,61 @@ class _WarehouseScreenState extends State<WarehouseScreen> {
             .toList();
       }
     });
+    if (selectedRecipeSorting == "Descending Order") {
+      filteredRecipes.sort((a, b) => b.recipeName.compareTo(a.recipeName));
+    }
   }
 
-  void _filterIngredients(String query) {
+  void _filterRecipe(
+      String selectingRecipeFiltering, String selectingRecipeSorting) {
+    setState(() {
+      selectedRecipeFiltering = selectingRecipeFiltering;
+      selectedRecipeSorting = selectingRecipeSorting;
+    });
+    if (selectedRecipeSorting == "Ascending Order") {
+      switch (selectingRecipeFiltering) {
+        case "Recipe Name":
+          filteredRecipes.sort((a, b) => a.recipeName.compareTo(b.recipeName));
+          break;
+        case "Total Time":
+          filteredRecipes.sort((a, b) => a.totalTime.compareTo(b.totalTime));
+          break;
+        case "Serving Amount":
+          filteredRecipes.sort((a, b) => a.servings.compareTo(b.servings));
+          break;
+        case "Rating":
+          filteredRecipes.sort((a, b) => a.stars.compareTo(b.stars));
+          break;
+        case "Selling Amount":
+          filteredRecipes.sort((a, b) => a.numOfOrder.compareTo(b.numOfOrder));
+          break;
+        default:
+          filteredRecipes.sort((a, b) => a.recipeName.compareTo(b.recipeName));
+      }
+    } else {
+      switch (selectingRecipeFiltering) {
+        case "Recipe Name":
+          filteredRecipes.sort((a, b) => b.recipeName.compareTo(a.recipeName));
+          break;
+        case "Total Time":
+          filteredRecipes.sort((a, b) => b.totalTime.compareTo(a.totalTime));
+          break;
+        case "Serving Amount":
+          filteredRecipes.sort((a, b) => b.servings.compareTo(a.servings));
+          break;
+        case "Rating":
+          filteredRecipes.sort((a, b) => b.stars.compareTo(a.stars));
+          break;
+        case "Selling Amount":
+          filteredRecipes.sort((a, b) => b.numOfOrder.compareTo(a.numOfOrder));
+          break;
+        default:
+          filteredRecipes.sort((a, b) => b.recipeName.compareTo(a.recipeName));
+      }
+    }
+  }
+
+  void _searchIngredients(String query) {
     setState(() {
       filteredIngredients = ingredients
           .where((ingredient) => ingredient.ingredientName
@@ -116,6 +188,55 @@ class _WarehouseScreenState extends State<WarehouseScreen> {
               .contains(query.toLowerCase()))
           .toList();
     });
+    if (selectedIngredientSorting == "Descending Order") {
+      filteredIngredients
+          .sort((a, b) => b.ingredientName.compareTo(a.ingredientName));
+    }
+  }
+
+  void _filterIngredient(
+      String selectingIngredientFiltering, String selectingIngredientSorting) {
+    setState(() {
+      selectedIngredientFiltering = selectingIngredientFiltering;
+      selectedIngredientSorting = selectingIngredientSorting;
+    });
+    if (selectedIngredientSorting == "Ascending Order") {
+      switch (selectedIngredientFiltering) {
+        case "Stock Name":
+          filteredIngredients
+              .sort((a, b) => a.ingredientName.compareTo(b.ingredientName));
+          break;
+        case "Quantity":
+          filteredIngredients.sort((a, b) => a.quantity.compareTo(b.quantity));
+          break;
+        case "Expiration Date":
+          filteredIngredients.sort((a, b) =>
+              convertExpirationDate(a.expirationStatus)
+                  .compareTo(convertExpirationDate(b.expirationStatus)));
+          break;
+        default:
+          filteredIngredients
+              .sort((a, b) => a.ingredientName.compareTo(b.ingredientName));
+      }
+    } else {
+      switch (selectedIngredientFiltering) {
+        case "Stock Name":
+          filteredIngredients
+              .sort((a, b) => b.ingredientName.compareTo(a.ingredientName));
+          break;
+        case "Quantity":
+          filteredIngredients.sort((a, b) => b.quantity.compareTo(a.quantity));
+          break;
+        case "Expiration Date":
+          filteredIngredients.sort((a, b) =>
+              convertExpirationDate(b.expirationStatus)
+                  .compareTo(convertExpirationDate(a.expirationStatus)));
+          break;
+        default:
+          filteredIngredients
+              .sort((a, b) => b.ingredientName.compareTo(a.ingredientName));
+      }
+    }
   }
 
   @override
@@ -195,16 +316,52 @@ class _WarehouseScreenState extends State<WarehouseScreen> {
                         BakingUpSearchBar(
                           hintText: 'Search Recipe',
                           controller: _searchRecipeController,
-                          onChanged: _filterRecipes,
+                          onChanged: _searchRecipes,
                         ),
                       if (tabIndex == 2)
                         BakingUpSearchBar(
                           hintText: 'Search Ingredient',
                           controller: _searchIngredientController,
-                          onChanged: _filterIngredients,
+                          onChanged: _searchIngredients,
                         ),
                       const SizedBox(width: 12),
-                      const BakingUpFilterTwoButton()
+                      GestureDetector(
+                          onTap: () {
+                            showModalBottomSheet<void>(
+                              context: context,
+                              backgroundColor:
+                                  const Color.fromRGBO(253, 253, 253, 1),
+                              shape: const RoundedRectangleBorder(
+                                // Adding shape property here
+                                borderRadius: BorderRadius.only(
+                                  topLeft: Radius.circular(40.0),
+                                  topRight: Radius.circular(40.0),
+                                ),
+                              ),
+                              builder: (BuildContext context) {
+                                return tabIndex == 1
+                                    ? BakingUpFilterModalBottom(
+                                        optionsOne: recipeFilterList,
+                                        optionOneName: "Filter by",
+                                        defaultFilteringValue:
+                                            selectedRecipeFiltering,
+                                        defaultSortingValue:
+                                            selectedRecipeSorting,
+                                        filterFunction: _filterRecipe,
+                                      )
+                                    : BakingUpFilterModalBottom(
+                                        optionsOne: ingredientFilterList,
+                                        optionOneName: "Filter by",
+                                        defaultFilteringValue:
+                                            selectedIngredientFiltering,
+                                        defaultSortingValue:
+                                            selectedIngredientSorting,
+                                        filterFunction: _filterIngredient,
+                                      );
+                              },
+                            );
+                          },
+                          child: const BakingUpFilterTwoButton())
                     ],
                   ),
                 ),
