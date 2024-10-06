91cb7fb46eb2c7933e05640180017f37b0106826
bbbung <kanyapat.mekv@kmutt.ac.th>
2024-10-06 14:58:43 +0700

    [ADD] user_info model

---
+++ lib/models/user_info.dart
@@ -0,0 +1,66 @@
+import 'package:json_annotation/json_annotation.dart';
+
+part 'user_info.g.dart';
+
+@JsonSerializable()
+  class UserInfoResponse {
+    final int status;
+    final String message;
+    final UserInfoData data;
+
+    UserInfoResponse({
+      required this.status,
+      required this.message,
+      required this.data,
+    });
+
+    factory UserInfoResponse.fromJson(Map<String, dynamic> json) =>
+        _$UserInfoResponseFromJson(json);
+      Map<String, dynamic> toJson() => _$UserInfoResponseToJson(this);
+  }
+
+  @JsonSerializable()
+  class UserInfoData {
+    @JsonKey(name: 'first_name')
+    final String firstName;
+    @JsonKey(name: 'last_name')
+    final String lastName;
+    final String tel;
+    @JsonKey(name: 'store_name')
+    final String storeName;
+    @JsonKey(name: 'production_queue')
+    final List<ProductionQueue> productionQueue;
+
+    UserInfoData({
+      required this.firstName,
+      required this.lastName,
+      required this.tel,
+      required this.storeName,
+      required this.productionQueue,
+    });
+
+    factory UserInfoData.fromJson(Map<String, dynamic> json) =>
+        _$UserInfoDataFromJson(json);
+      Map<String, dynamic> toJson() => _$UserInfoDataToJson(this);
+  }
+
+  @JsonSerializable()
+  class ProductionQueue {
+    @JsonKey(name: 'order_index')
+    final String orderIndex;
+    final String name;
+    final String quantity;
+    @JsonKey(name: 'pick_up_date')
+    final String pickUpDate;
+
+    ProductionQueue({
+      required this.orderIndex,
+      required this.name,
+      required this.quantity,
+      required this.pickUpDate,
+    });
+
+    factory ProductionQueue.fromJson(Map<String, dynamic> json) =>
+        _$ProductionQueueFromJson(json);
+      Map<String, dynamic> toJson() => _$ProductionQueueToJson(this);
+  }
---
+++ lib/models/user_info.g.dart
@@ -0,0 +1,56 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'user_info.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+UserInfoResponse _$UserInfoResponseFromJson(Map<String, dynamic> json) =>
+    UserInfoResponse(
+      status: (json['status'] as num).toInt(),
+      message: json['message'] as String,
+      data: UserInfoData.fromJson(json['data'] as Map<String, dynamic>),
+    );
+
+Map<String, dynamic> _$UserInfoResponseToJson(UserInfoResponse instance) =>
+    <String, dynamic>{
+      'status': instance.status,
+      'message': instance.message,
+      'data': instance.data,
+    };
+
+UserInfoData _$UserInfoDataFromJson(Map<String, dynamic> json) => UserInfoData(
+      firstName: json['first_name'] as String,
+      lastName: json['last_name'] as String,
+      tel: json['tel'] as String,
+      storeName: json['store_name'] as String,
+      productionQueue: (json['production_queue'] as List<dynamic>)
+          .map((e) => ProductionQueue.fromJson(e as Map<String, dynamic>))
+          .toList(),
+    );
+
+Map<String, dynamic> _$UserInfoDataToJson(UserInfoData instance) =>
+    <String, dynamic>{
+      'first_name': instance.firstName,
+      'last_name': instance.lastName,
+      'tel': instance.tel,
+      'store_name': instance.storeName,
+      'production_queue': instance.productionQueue,
+    };
+
+ProductionQueue _$ProductionQueueFromJson(Map<String, dynamic> json) =>
+    ProductionQueue(
+      orderIndex: json['order_index'] as String,
+      name: json['name'] as String,
+      quantity: json['quantity'] as String,
+      pickUpDate: json['pick_up_date'] as String,
+    );
+
+Map<String, dynamic> _$ProductionQueueToJson(ProductionQueue instance) =>
+    <String, dynamic>{
+      'order_index': instance.orderIndex,
+      'name': instance.name,
+      'quantity': instance.quantity,
+      'pick_up_date': instance.pickUpDate,
+    };
