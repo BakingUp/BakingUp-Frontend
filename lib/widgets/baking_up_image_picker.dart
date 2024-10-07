7dfa7db4814bb624daa422851d351f144ab9e594
Pawin Nakvisai <pawinnak2@gmail.com>
2024-10-08 00:03:32 +0700

    [ADD] Image picker bottom sheet

--- lib/widgets/baking_up_image_picker.dart
+++ lib/widgets/baking_up_image_picker.dart
@@ -1,4 +1,5 @@
 import 'package:bakingup_frontend/constants/colors.dart';
+import 'package:bakingup_frontend/widgets/baking_up_image_picker_bottom_sheet.dart';
 import 'package:flutter/material.dart';
 
 class BakingUpImagePicker extends StatelessWidget {
@@ -6,35 +7,40 @@ class BakingUpImagePicker extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: greyColor,
-      margin: const EdgeInsets.all(12.0),
-      width: MediaQuery.of(context).size.width,
-      height: MediaQuery.of(context).size.width / 2,
-      child: Center(
-        child: Container(
-          padding: const EdgeInsets.symmetric(horizontal: 5, vertical: 1),
-          decoration: BoxDecoration(
-            color: whiteColor,
-            borderRadius: BorderRadius.circular(30),
-          ),
-          child: Row(
-            mainAxisSize: MainAxisSize
-                .min, // This makes the Row only take as much space as needed
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const Text(
-                "Add image",
-                style: TextStyle(
-                  fontSize: 12,
-                  fontFamily: 'Inter',
-                  fontStyle: FontStyle.normal,
-                  fontWeight: FontWeight.w300,
+    return GestureDetector(
+      onTap: () {
+        BakingUpImagePickerBottomSheet.show(context);
+      },
+      child: Container(
+        color: greyColor,
+        margin: const EdgeInsets.all(12.0),
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.width / 2,
+        child: Center(
+          child: Container(
+            padding: const EdgeInsets.symmetric(horizontal: 5, vertical: 1),
+            decoration: BoxDecoration(
+              color: whiteColor,
+              borderRadius: BorderRadius.circular(30),
+            ),
+            child: Row(
+              mainAxisSize: MainAxisSize
+                  .min,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text(
+                  "Add image",
+                  style: TextStyle(
+                    fontSize: 12,
+                    fontFamily: 'Inter',
+                    fontStyle: FontStyle.normal,
+                    fontWeight: FontWeight.w300,
+                  ),
                 ),
-              ),
-              const SizedBox(width: 4),
-              Image.asset('assets/icons/add.png'),
-            ],
+                const SizedBox(width: 4),
+                Image.asset('assets/icons/add.png'),
+              ],
+            ),
           ),
         ),
       ),
